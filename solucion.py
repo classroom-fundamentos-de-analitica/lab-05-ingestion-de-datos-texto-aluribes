4b49223be3eaf44616b5e8b89d4ed52b54ca86a5
Alejandra Uribe Sierra <92702107+aluribes@users.noreply.github.com>
2024-09-22 19:41:14 -0500

    Intento lab 8

--- solucion.py
+++ solucion.py
@@ -45,9 +45,9 @@ def create_test_and_train_dataset():
 
 create_test_and_train_dataset()
 
-# df = pd.read_csv('train_dataset.csv')
-# print(df["sentiment"].value_counts())
+df = pd.read_csv('train_dataset.csv')
+print(df["sentiment"].value_counts())
 
-# df2 = pd.read_csv('test_dataset.csv')
-# print(df2["sentiment"].value_counts())
+df2 = pd.read_csv('test_dataset.csv')
+print(df2["sentiment"].value_counts())
 
